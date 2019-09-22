719fbe416b2265833b391337bcf34f2611e3eaf6
Nemanja Jakonić <nemanjajakonic@gmail.com>
2019-09-22 07:49:41 +0200

    modified:   weather.js

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`)
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`)
 
     const responseData = await response.json();
 
